68cb6411bce7537cdc5c32f7c7ae6c7754e587e3
sanya-padalko <apadalko90@gmail.com>
2025-09-11 01:15:29 +0300

    Update matrix.cpp
    Умеем присваивать некоторой ячейке треугольной матрице конкретное значение (0-индексация)

--- matrix.cpp
+++ matrix.cpp
@@ -5,11 +5,11 @@
 
 using namespace std;
 
-struct sizes {
+struct SIZE {
     int n, m;
 };
 
-ERRORS print(int* a, sizes* sz) {
+ERRORS print(int* a, SIZE* sz) {
     my_assert(!a, NULLPTR);
     my_assert(!sz, NULLPTR);
 
@@ -25,7 +25,7 @@ ERRORS print(int* a, sizes* sz) {
     return NOTHING;
 }
 
-ERRORS sum(int* a, int* b, int* ans, sizes* sz) {
+ERRORS sum(int* a, int* b, int* ans, SIZE* sz) {
     my_assert(!a, NULLPTR);
     my_assert(!b, NULLPTR);
     my_assert(!ans, NULLPTR);
@@ -44,7 +44,7 @@ ERRORS sum(int* a, int* b, int* ans, sizes* sz) {
     return NOTHING;
 }
 
-ERRORS sub(int* a, int* b, int* ans, sizes* sz) {
+ERRORS sub(int* a, int* b, int* ans, SIZE* sz) {
     my_assert(!a, NULLPTR);
     my_assert(!b, NULLPTR);
     my_assert(!ans, NULLPTR);
@@ -64,7 +64,7 @@ ERRORS sub(int* a, int* b, int* ans, sizes* sz) {
     return NOTHING;
 }
 
-ERRORS mul(int* a, sizes* sz, int k) {
+ERRORS mul(int* a, SIZE* sz, int k) {
     my_assert(!a, NULLPTR);
     my_assert(!sz, NULLPTR);
     
@@ -82,6 +82,16 @@ ERRORS mul(int* a, sizes* sz, int k) {
     return NOTHING;
 }
 
+ERRORS change_tr(int* a, int n, int i, int j, int x) {
+    my_assert(!a, NULLPTR);
+    my_assert((i >= n || j > i), INDEX_ERR);
+
+    int real_index = i * (i + 1) / 2 + j;
+    *(a + real_index) = x;
+
+    return NOTHING;
+}
+
 int main() {
     int a[3][2] = {{1, 2},
                    {3, 4},
@@ -91,10 +101,11 @@ int main() {
                    {5, 6}};
     int c[3][2] = {{0}};
 
-    sizes sz;
+    SIZE sz;
     sz.n = 3;
     sz.m = 2;
 
+    /*
     kill_main(sum((int*)a, (int*)b, (int*)c, &sz));
 
     kill_main(print((int*)c, &sz));
@@ -106,4 +117,12 @@ int main() {
     kill_main(mul((int*)a, &sz, 5));
 
     kill_main(print((int*)a, &sz));
+    */
+
+    int p[100] = {0};
+    change_tr(p, 5, 4, 4, 10);
+
+    for (int i = 0; i < 6 * 5 / 2; ++i) {
+        printf("%d ", p[i]);
+    }
 }
