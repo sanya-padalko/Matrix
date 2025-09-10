cea7eeac75f871e90df7827696caed4ae069d722
sanya-padalko <apadalko90@gmail.com>
2025-09-11 01:14:34 +0300

    Update errors.cpp

--- errors.cpp
+++ errors.cpp
@@ -4,7 +4,8 @@
 
 const char* err_msg[] = {"Pointer went equal NULL", 
                          "Variable went equal NAN",
-                         "The digit didn't become finite"};
+                         "The digit didn't become finite",
+                         "Index out of range"};
 
 void print_err_message(const char *file_name, const char* func_name, const int n_line,
                        const int error_code) {
